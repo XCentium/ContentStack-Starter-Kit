ef66d1524a0cd4982e7464123644ca4a0ee05559
André Moraes <andrelom@gmail.com>
2023-08-03 19:08:23 -0300

    Create http.ts

---
+++ packages/lib/src/http.ts
@@ -0,0 +1,26 @@
+export function createURL(
+  pathname: string,
+  options?: {
+    origin?: string
+    search?: Record<string, any>
+  },
+): URL {
+  let origin = options?.origin
+
+  if (!origin) {
+    if (typeof window === 'undefined') {
+      origin = process.env.NEXT_BASE_URL
+    } else {
+      origin = process.env.NEXT_PUBLIC_BASE_URL
+    }
+  }
+
+  const url = new URL(pathname, origin)
+  const search = options?.search ?? {}
+
+  for (const [key, value] of Object.entries(search)) {
+    url.searchParams.append(key, value)
+  }
+
+  return url
+}
