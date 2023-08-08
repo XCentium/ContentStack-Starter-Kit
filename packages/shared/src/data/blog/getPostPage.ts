9afa00e80eb75f84c625951ad55b3f180efa9610
André Moraes <andrelom@gmail.com>
2023-08-08 14:31:34 -0300

    Update getPostPage.ts

--- packages/shared/src/data/blog/getPostPage.ts
+++ packages/shared/src/data/blog/getPostPage.ts
@@ -10,7 +10,7 @@ export type PostPageData = Contentstack.Item<{
   open_graph: Contentstack.Globals.OpenGraph
 }>
 
-export const generateMetadata = createMetadataGenerator('page_post', blog.api)
+export const generateMetadata = createMetadataGenerator('page_post', '/posts', blog.api)
 
 export default async function getPostPage({
   path,
