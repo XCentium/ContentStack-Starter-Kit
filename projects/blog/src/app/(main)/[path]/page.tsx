0724a848c129e13ca4065f3b1b26db6669b7445e
André Moraes <andrelom@gmail.com>
2023-08-08 14:56:37 -0300

    Update page.tsx

--- projects/blog/src/app/(main)/[path]/page.tsx
+++ projects/blog/src/app/(main)/[path]/page.tsx
@@ -9,7 +9,7 @@ export { dynamic, revalidate } from '@/ssr'
 export { generateMetadata }
 
 export default async function Page({ params, searchParams }: Core.Page<{ path: string }>) {
-  const result = await getGenericPage({ path: params.path, preview: searchParams })
+  const result = await getGenericPage({ ...params, preview: { ...searchParams } })
 
   if (!result.ok || !result.data) {
     return notFound()
